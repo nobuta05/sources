3a60e8f6e7464c328302bf8566cf04628676ad28
nobuta05 <nobuta05@gmail.com>
2018-04-03 15:53:34 +0900

    modified mem

--- mem.jl
+++ mem.jl
@@ -3,42 +3,35 @@ srand(100);
 
 # ひとまずGMMを想定して実装
 # 詳しい計算はpdf『MEMAlgorithm.pdf』を参照
-function mem(d::Sampleable, x=nothing, N::Int64=10)
+
+# function mem(d::Sampleable, x=nothing, N::Int64=10)
+function mem(d, x=nothing, N::Int64=5)
   comps = components(d);
   prior = probs(d);
   K = length(prior);
   μs = map(comp->mean(comp), comps);
   σs = map(comp->std(comp), comps);
+  σ2s = σs.^2;
   
-  function mem_search(d, x)
-    Loop = 100;
-    EPS = 10e-7;
+  function mem_search(d, xinit)
+    const Loop = 100;
+    const EPS = 10e-5;
+    x = 0.0;
 
     for i in 1:Loop
       # E-Step
       ps = map(l->pdf(comps[l], x), 1:K) ./ pdf(d, x);
+      
       # M-Step
-      numerator = sum(map(l->ps[l]*μs[l]/(σs[l]^2), 1:K));
-      denominator = sum(map(l->ps[l]/(σs[l]^2), 1:K));
-      x_nxt = numerator / denominator;
+      numerator = sum( (ps.*μs)./σ2s );
+      denominator = sum(ps./σ2s);
+      xnxt = numerator / denominator;
 
-      #= Output
-      sum_ps_logcomps(x) = dot(ps, log.(pdf.(comps, x)));
-      sum_ps_logπs = dot(ps, log.(prior));
-      sum_ps_logps = dot(ps, log.(ps));
-      surrogate(x) = sum_ps_logcomps(x) + sum_ps_logπs - sum_ps_logps;
-      xrng = linspace(-5,5,500);
-      plot(xrng, log.(pdf.(d, xrng)), label="log pdf", ylim=(-10,-1));
-      plot!(xrng, surrogate.(xrng), label="surrogate", ylim=(-10,-1));
-      vline!([x], label="current", lw=2);
-      vline!([x_nxt], label="optimum", lw=2);
-      savefig("i_"*string(i)*".pdf")
-      =#
-
-      if norm(x_nxt - x) < EPS
+      if norm(xnxt - x) < EPS
+        x = xnxt;
         break;
       else
-        x = x_nxt;
+        x = xnxt;
       end
     end
     return x;
@@ -46,8 +39,7 @@ function mem(d::Sampleable, x=nothing, N::Int64=10)
 
   if x == nothing
     xs = rand(d, N);
-    modes = zeros(N);
-    modes = map(x->mem_search(d,x), xs);
+    modes = mem_search.(d, xs);
     pdfs = pdf.(d, modes);
     ind = sortperm(pdfs, rev=true)[1];
     mode = modes[ind];
